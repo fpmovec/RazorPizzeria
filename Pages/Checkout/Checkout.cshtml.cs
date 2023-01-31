13c6749f4201d87fdd5700400ae7823ef0a25291
fpmovec <10.maks.10K@gmail.com>
2023-01-31 22:07:12 +0300

    Finished

--- Pages/Checkout/Checkout.cshtml.cs
+++ Pages/Checkout/Checkout.cshtml.cs
@@ -1,12 +1,42 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPizzeria.Data;
+using RazorPizzeria.Models;
 
 namespace RazorPizzeria.Pages.Checkout
 {
+    [BindProperties(SupportsGet = true)]
     public class CheckoutModel : PageModel
     {
+        public string? PizzaName { get; set; }
+        public float PizzaPrice { get; set; }
+        public string ImageTitle { get; set; }
+
+        private readonly ApplicationDbContext _context;
+        public CheckoutModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public void OnGet()
         {
+            if (string.IsNullOrWhiteSpace(PizzaName))
+            {
+                PizzaName = "Custom";
+            }
+            if (string.IsNullOrWhiteSpace(ImageTitle))
+            {
+                ImageTitle = "Create";
+            }
+
+            PizzaOrder order = new PizzaOrder
+            {
+                PizzaName = this.PizzaName,
+                BasePrice = this.PizzaPrice
+            };
+
+            _context.PizzaOrders.Add(order);
+            _context.SaveChanges();
+
         }
     }
 }
--- Pages/Pizza.cshtml.cs
+++ Pages/Pizza.cshtml.cs
@@ -1,10 +1,90 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPizzeria.Models;
 
 namespace RazorPizzeria.Pages
 {
     public class PizzaModel : PageModel
     {
+        public List<PizzasModel> fakePizzaDB = new List<PizzasModel>()
+        {
+            new PizzasModel()
+            {
+                ImageName="Margerita",
+                PizzaName="Margerita",
+                BasePrice=2,
+                TomatoSauce = true,
+                TotalPrice = 4
+            },
+            new PizzasModel()
+            {
+                ImageName="Bolognese",
+                PizzaName="Bolognese",
+                BasePrice =2,
+                TomatoSauce=true,
+                Beef=true,
+                TotalPrice = 5
+            },
+             new PizzasModel()
+            {
+                ImageName="Hawaiian",
+                PizzaName="Hawaiian",
+                BasePrice=2,
+                TomatoSauce = true,
+                TotalPrice = 4
+            },
+            new PizzasModel()
+            {
+                ImageName="Carbonara",
+                PizzaName="Carbonara",
+                BasePrice =2,
+                TomatoSauce=true,
+                Beef=true,
+                TotalPrice = 5
+            },
+             new PizzasModel()
+            {
+                ImageName="MeatFeast",
+                PizzaName="MeatFeast",
+                BasePrice=2,
+                TomatoSauce = true,
+                TotalPrice = 4
+            },
+            new PizzasModel()
+            {
+                ImageName="Mushroom",
+                PizzaName="Mushroom",
+                BasePrice =2,
+                TomatoSauce=true,
+                Beef=true,
+                TotalPrice = 5
+            },
+             new PizzasModel()
+            {
+                ImageName="Pepperoni",
+                PizzaName="Pepperoni",
+                BasePrice=2,
+                TomatoSauce = true,
+                TotalPrice = 4
+            },
+            new PizzasModel()
+            {
+                ImageName="Seafood",
+                PizzaName="Seafood",
+                BasePrice =2,
+                TomatoSauce=true,
+                Beef=true,
+                TotalPrice = 5
+            },
+             new PizzasModel()
+             {
+                ImageName="Vegetarian",
+                PizzaName="Vegetarian",
+                BasePrice=2,
+                TomatoSauce = true,
+                TotalPrice = 4
+             }
+        };
         public void OnGet()
         {
         }
